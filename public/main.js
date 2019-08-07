699e4677217ac9bebcfc5abf1320bb250a669bba
nickryanweber <nicholasryanweber@gmail.com>
2019-08-07 16:01:04 -0400

    explorer mdoe done

--- public/main.js
+++ public/main.js
@@ -1,7 +1,58 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+let player1Hand = []
+let player2Hand = []
+const dealtCards = document.querySelector('.selected-card')
+const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+const values = [
+  'Ace',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'Jack',
+  'Queen',
+  'King'
+]
+let deck = []
+
+const createDeck = () => {
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      const newCard = `${values[j]} of ${suits[i]}`
+      deck.push(newCard)
+      // console.log(newCard)
+    }
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const shuffleDeck = () => {
+  let arrayLength = deck.length
+  let holdVariable = ''
+
+  for (let endSelector = arrayLength - 1; endSelector > 0; endSelector--) {
+    const randomNumber = Math.floor(Math.random() * arrayLength)
+    // console.log(deck[randomNumber])
+    holdVariable = deck[endSelector]
+    deck[endSelector] = deck[randomNumber]
+    deck[randomNumber] = holdVariable
+  }
+}
+
+const dealCards = () => {
+  while (dealtCards.hasChildNodes()) {
+    dealtCards.removeChild(dealtCards.firstChild)
+  }
+  const card = document.createElement('p')
+  card.textContent = deck.pop()
+  dealtCards.appendChild(card)
+}
+
+// player.push(deck.pop())
+
+document.addEventListener('DOMContentLoaded', createDeck)
+document.addEventListener('DOMContentLoaded', shuffleDeck)
+document.querySelector('button').addEventListener('click', dealCards)
