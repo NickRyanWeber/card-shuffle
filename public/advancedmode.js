ff5531dcb751e3f8243e734aadbeb6ee9e880f72
nickryanweber <nicholasryanweber@gmail.com>
2019-08-07 23:41:51 -0400

    got functionality to work but hitting an error where after a draw the game gets stuck in a loop

--- public/advancedmode.js
+++ public/advancedmode.js
@@ -1,5 +1,4 @@
-const suits = ['Clubs'] // for testing
-// const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 const values = [
   ['2', 2],
   ['3', 3],
@@ -12,14 +11,22 @@ const values = [
   ['10', 10],
   ['Jack', 11],
   ['Queen', 12],
-  ['King', 13]
+  ['King', 13],
+  ['Ace', 14]
 ]
 let deck = []
 let holdVariable = ''
 let holdArray = []
 
 const playerHandDisplay = document.querySelector('.player > .current-card')
+const playerWinnerArea = document.querySelector('.player > .hand-winner-area')
+const playerCardCount = document.querySelector('.player > .cards-in-hand')
+
 const computerHandDisplay = document.querySelector('.computer > .current-card')
+const computerWinnerArea = document.querySelector(
+  '.computer > .hand-winner-area'
+)
+const computerCardCount = document.querySelector('.computer > .cards-in-hand')
 
 let playerOffset
 let computerOffset
@@ -30,62 +37,72 @@ const createDealSplitDeck = () => {
     for (let j = 0; j < values.length; j++) {
       const newCard = [`${values[j][0]} of ${suits[i]}`, values[j][1]]
       deck.push(newCard)
-      // console.log(newCard)
     }
   }
 
   const arrayLength = deck.length
 
-  // shuffle
-  // for (let endSelector = arrayLength - 1; endSelector > 0; endSelector--) {
-  //   const randomNumber = Math.floor(Math.random() * arrayLength)
-  //   // console.log(deck[randomNumber])
-  //   holdVariable = deck[endSelector]
-  //   deck[endSelector] = deck[randomNumber]
-  //   deck[randomNumber] = holdVariable
-  // }
+  //shuffle
+  for (let endSelector = arrayLength - 1; endSelector > 0; endSelector--) {
+    const randomNumber = Math.floor(Math.random() * arrayLength)
+    holdVariable = deck[endSelector]
+    deck[endSelector] = deck[randomNumber]
+    deck[randomNumber] = holdVariable
+  }
 
   playerOffset = deck.length / 2 - 1
   computerOffset = playerOffset + 1
   arrayOffset = computerOffset - playerOffset + 1
-  // console.log(deck)
+  updateCardCount()
 }
 
 const dealCards = () => {
-  console.log(deck)
-  console.log(playerOffset, computerOffset)
-  console.log(deck[5], deck[6])
-  console.log(playerOffset, computerOffset)
+  console.log(
+    `Player Offset = ${playerOffset} & Computer Offset = ${computerOffset}`
+  )
+  playerHandDisplay.textContent = deck[playerOffset][0]
+  computerHandDisplay.textContent = deck[computerOffset][0]
+
   if (deck[playerOffset][1] > deck[computerOffset][1]) {
     playerWin()
   } else if (deck[playerOffset][1] < deck[computerOffset][1]) {
     computerWin()
-  } else {
+  } else if (deck[playerOffset][1] === deck[computerOffset][1]) {
     draw()
+  } else {
+    console.log(
+      "something broke, the cards aren't greater than, equal to, or less than"
+    )
   }
-  console.log(deck)
 }
 
 const playerWin = () => {
   console.log('player win')
+  playerWinnerArea.textContent = 'Wins'
+  computerWinnerArea.textContent = 'Loses'
   holdArray = deck.splice(playerOffset, arrayOffset)
   holdArray.forEach(element => {
     deck.unshift(element)
   })
   playerOffset++
-  computerOffset--
+  computerOffset++
   arrayOffset = computerOffset - playerOffset + 1
+  updateCardCount()
 }
 
 const computerWin = () => {
   console.log('computer win')
+  playerWinnerArea.textContent = 'Loses'
+  computerWinnerArea.textContent = 'Wins'
   holdArray = deck.splice(playerOffset, arrayOffset)
   holdArray.forEach(element => {
     deck.push(element)
   })
-  playerOffset++
+  playerOffset--
   computerOffset--
   arrayOffset = computerOffset - playerOffset + 1
+  updateCardCount()
+  checkWin((computerCardCount.textContent = deck.length - playerOffset - 1))
 }
 
 const draw = () => {
@@ -94,5 +111,36 @@ const draw = () => {
   playerOffset++
 }
 
+const updateCardCount = () => {
+  playerCardCount.textContent = playerOffset + 1
+  computerCardCount.textContent = deck.length - playerOffset - 1
+}
+
+const checkWin = (score, winner) => {
+  if (score === deck.length) {
+    if (winner === 'Player') {
+      document.querySelector('.winner-message').textContent = 'Player Wins'
+    } else {
+      document.querySelector('.winner-message').textContent = 'Computer Wins'
+    }
+    document.querySelector('#modal').classList.remove('modal-hide')
+    document.querySelector('#modal').classList.add('modal-show')
+  }
+}
+
+const reset = () => {
+  deck = []
+  createDealSplitDeck()
+  document.querySelector('#modal').classList.remove('modal-show')
+  document.querySelector('#modal').classList.add('modal-hide')
+  document.querySelector('.player > .current-card').textContent =
+    'Deal for a card'
+  document.querySelector('.player > .hand-winner-area').textContent = '\xa0'
+  document.querySelector('.computer > .current-card').textContent =
+    'Deal for a card'
+  document.querySelector('.computer > .hand-winner-area').textContent = '\xa0'
+}
+
 document.addEventListener('DOMContentLoaded', createDealSplitDeck)
-document.querySelector('button').addEventListener('click', dealCards)
+document.querySelector('.deal').addEventListener('click', dealCards)
+document.querySelector('.play-again').addEventListener('click', reset)
